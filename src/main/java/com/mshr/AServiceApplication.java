f8d95ad5fcefd51616be9da6f8389c7452ef1d55
rakibidb <rakibhossan55@gmail.com>
2018-09-02 10:17:10 +0600

    Initial commit

---
+++ src/main/java/com/mshr/AServiceApplication.java
@@ -0,0 +1,32 @@
+package com.mshr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@SpringBootApplication
+public class AServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AServiceApplication.class, args);
+
+	}
+
+	@GetMapping("/my/{id}")
+	public Person g(@PathVariable("id") Integer id) {
+
+		String url = "http://localhost:8081/persons/"+ id;
+
+		RestTemplate restTemplate = new RestTemplate();
+
+		Person p = restTemplate.getForObject(url, Person.class);
+
+		System.out.println("Id " + id + " :" + p.getName());
+		return p;
+	}
+
+}
